186f2d0ce8aa8d383d7c6d518f8ea36710084b02
Adrian Morgenthal <adrian.morgenthal92@gmail.com>
2024-07-05 08:57:36 +0200

    Changes 6/3/2023 AM
    Update comments, refactor euler method

--- src/main/java/rancore/math/Euler.java
+++ src/main/java/rancore/math/Euler.java
@@ -1,16 +1,19 @@
 package rancore.math;
+
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * <p><u><b>Class Name:</b></u></p>
  * <p>Euler</p>
  *
  * <p><u><b>Description:</b></u></p>
- * <p>The Euler class is an implementation of methods to calculate Euler's number e.</p>
+ * <p>The Euler class provides methods to calculate Euler's number (e) to a specified precision.</p>
  *
  * <p><u><b>Instance Variables:</b></u></p>
- * <p><code>euler</code>: A BigDecimal representing Euler's number.</p>
+ * <p><code>euler</code>: A BigDecimal representing the calculated value of Euler's number.</p>
  *
  * <p><u><b>Constructor:</b></u></p>
  * <p><code>Euler():</code> Initializes the Euler object by calculating the value of e using a default precision.</p>
@@ -19,40 +22,33 @@ import java.math.MathContext;
  * <p><u><b>Methods:</b></u></p>
  * <p><code>euler():</code> Calculates and returns the value of e with a default precision.</p>
  * <p><code>euler(int n):</code> Calculates and returns the value of e with the specified precision.</p>
- *
- * <p><u><b>License:</b></u></p>
- * <p>Version 1.0</p>
- * <p>2023/05/20</p>
- * <p>Attribution: <a href="https://creativecommons.org/licenses/by/4.0/">CC BY</a></p>
- * <p>Adrian Morgenthal <a href="https://github.com/Voraxx">Github</a></p>
  */
-
 public class Euler {
 
-    private static BigDecimal euler = BigDecimal.ONE;
+    private static BigDecimal euler;
 
     /**
      * Calculates Euler's number with a default value of n=1000.
      *
-     * @return The value of Euler's number.
+     * @return The value of Euler's number as a BigDecimal.
+     * <p>
+     * This method provides a convenient way to calculate Euler's number using
+     * a default iteration count, ensuring a reasonably high precision.
      */
     public static BigDecimal euler() {
         int n = 1000;
-        BigDecimal euler = BigDecimal.ONE;
-        BigDecimal faktor = BigDecimal.ONE;
-
-        for (int i = 1; i <= n; i++) {
-            faktor = faktor.divide(BigDecimal.valueOf(i), MathContext.DECIMAL128);
-            euler = euler.add(faktor);
-        }
-        return euler;
+        return euler(n);
     }
 
     /**
      * Calculates Euler's number with the specified value of n.
      *
      * @param n The number of iterations to perform the calculation.
-     * @return The value of Euler's number.
+     * @return The value of Euler's number as a BigDecimal.
+     * <p>
+     * This method uses a series expansion to calculate the value of Euler's number (e).
+     * The higher the value of n, the more accurate the result, as more terms are included
+     * in the series.
      */
     public static BigDecimal euler(int n) {
         BigDecimal euler = BigDecimal.ONE;
@@ -67,6 +63,9 @@ public class Euler {
 
     /**
      * Constructs a new Euler object and calculates Euler's number with the default value of n=1000.
+     * <p>
+     * This constructor allows the creation of an Euler object with the value of e pre-calculated
+     * to a default precision.
      */
     public Euler() {
         euler = euler();
@@ -76,9 +75,51 @@ public class Euler {
      * Constructs a new Euler object and calculates Euler's number with the specified value of n.
      *
      * @param n The number of iterations to perform the calculation.
+     * <p>
+     * This constructor allows the creation of an Euler object with the value of e pre-calculated
+     * to a specified precision.
      */
     public Euler(int n) {
         euler = euler(n);
+    }
+
+    /**
+     * Main method for testing the Euler class.
+     *
+     * @param args Command-line arguments (not used).
+     * <p>
+     * This method demonstrates the usage of the Euler class by calculating Euler's number
+     * and printing the results to the console.
+     */
+    public static void main(String[] args) {
+        int[] precisions = {10, 100, 1000, 10000, 100000};
+
+        System.out.println("Euler's Number Calculation");
+        System.out.println("==========================");
+
+        for (int precision : precisions) {
+            Instant start = Instant.now();
+            BigDecimal eulerValue = euler(precision);
+            Instant end = Instant.now();
+
+            Duration duration = Duration.between(start, end);
+
+            System.out.println("Precision: " + precision);
+            System.out.println("Value: " + eulerValue);
+            System.out.println("Time taken: " + duration.toMillis() + " milliseconds");
+            System.out.println("--------------------------");
+        }
+
+        // Additional test using the default constructor and method
+        Instant start = Instant.now();
+        BigDecimal eulerDefault = euler();
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+
+        System.out.println("Default Precision (1000)");
+        System.out.println("Value: " + eulerDefault);
+        System.out.println("Time taken: " + duration.toMillis() + " milliseconds");
+        System.out.println("==========================");
     }
 }
 
