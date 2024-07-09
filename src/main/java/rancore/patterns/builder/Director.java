40f00ad5987e69126c79d2ee44d0de126e60c2dc
Adrian Morgenthal <adrian.morgenthal92@gmail.com>
2024-07-09 20:51:42 +0200

    Implement pattern: Builder (forgot Director)

---
+++ src/main/java/rancore/patterns/builder/Director.java
@@ -0,0 +1,13 @@
+package rancore.patterns.builder;
+
+public class Director {
+    private Builder builder;
+
+    public void setBuilder(Builder builder) { this.builder = builder; }
+
+    public Product construct() {
+        builder.buildPart1();
+        builder.buildPart2();
+        return builder.getResult();
+    }
+}
