44429389c9dac9472170453062a0d9981da90244
Adrian Morgenthal <Adrian.Morgenthal@haw-hamburg.de>
2024-07-13 11:30:13 +0200

    Refactor ILogExecTime

--- src/main/java/rancore/annotation/ILogExecTime.java
+++
@@ -1,9 +0,0 @@
-package rancore.annotation;
-
-public interface ILogExecTime {
-    @LogExecTime(message = "Komplexe Operation dauerte:\t\t")
-    void complexOperation();
-
-    @LogExecTime(logToConsole = false)
-    String silentOperation();
-}
