e40742492dac9f6906160d60b2f2633f6f608988
Adrian Morgenthal <adrian.morgenthal92@gmail.com>
2024-07-18 06:27:19 +0200

    Add JavaDoc Template

---
+++ src/main/java/org/rancore/javadoc/Template.java
@@ -0,0 +1,53 @@
+package org.rancore.javadoc;
+
+/**
+ * <h1>Heading H1</h1>
+ * <h2>Heading H2</h2>
+ * <h3>Heading H3</h3>
+ * <p>This class represents a simple example of using HTML tags in JavaDocs.</p>
+ * <p>
+ * <b>bold</b>
+ * <i>italic</i>
+ * <u>underline</u>
+ * </p>
+ * <p>Unordered list:</p>
+ * <ul>
+ *   <li>Item Lorem</li>
+ *   <li>Item Ipsum</li>
+ *   <li>Item Dolor</li>
+ * </ul>
+ * <br>
+ * <p>Ordered list:</p>
+ * <ol>
+ *     <li>Item</li>
+ *     <li>Item</li>
+ *     <li>Item</li>
+ * </ol>
+ * <br>
+ * <p>Description list:</p>
+ * <dl>
+ *     <dt>Term 1</dt>
+ *     <dd>Description</dd>
+ *     <dt>Term 2</dt>
+ *     <dd>Description</dd>
+ * </dl>
+ * <p><code>inline code</code> and a block of preformatted text:</p>
+ * <pre>
+ * public static void main(String[] args) {
+ *     System.out.println("Hello, world!");
+ * }
+ * </pre>
+ * <a href="www.github.com/amaldoror">Link</a>
+ */
+public class Template {
+
+    /**
+     * <p><u><b>Class name</b></u></p>
+     * <p>Lorem</p>
+     * <p><u><b>Description</b></u></p>
+     * <p>Lorem ipsum dolor sit amet</p>
+     * <p><u><b>Usage</b></u></p>
+     * <p>java Lorem <directory> [--print-structure | -p] [--save-to-file | -s <output-path>]</p>
+     */
+    static class Lorem {}
+}
