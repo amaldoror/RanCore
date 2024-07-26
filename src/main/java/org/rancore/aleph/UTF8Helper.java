3eba9952d28fbe05c7f5a7071d069706e5930e48
Adrian Morgenthal <Adrian.Morgenthal@haw-hamburg.de>
2024-07-26 17:21:31 +0200

    Update UTF8Helper

--- src/main/java/org/rancore/aleph/UTF8Helper.java
+++ src/main/java/org/rancore/aleph/UTF8Helper.java
@@ -3,20 +3,27 @@ package org.rancore.aleph;
 import java.nio.charset.StandardCharsets;
 
 public class UTF8Helper {
-    static byte[] utf8Bytes = {72, 101, 108, 108, 111, 44, 32, 85, 84, 70, 45, 56, 33}; // "Hello, UTF-8!"
+    private static final byte[] utf8Bytes = {72, 101, 108, 108, 111, 44, 32, 85, 84, 70, 45, 56, 33}; // "Hello, UTF-8!"
+    private static final String COMPLETE =
+            " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    private static final String ALPHABET_SMALL = "abcdefghijklmnopqrstuvwxyz";
+    private static final String ALPHABET_CAPITAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBERS = "0123456789";
 
     public static void main(String[] args) {
-        //encodeString("abcdefghijklmnopqrstuvwxyz");
-        //encodeString("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        //encodeString("0123456789");
-        byte[] bytes = generateBytes(1000);
+        encodeString(ALPHABET_SMALL);
+        encodeString(ALPHABET_CAPITAL);
+        encodeString(NUMBERS);
+        encodeString(COMPLETE);
+        byte[] bytes = generateBytes(95);
         System.out.println(decodeBytes(bytes));
+        System.out.println(decodeBytes(utf8Bytes));
     }
 
     public static byte[] generateBytes(int n){
         byte[] b = new byte[n];
         for (int i = 0; i<n; i++){
-            b[i] = (byte)i;
+            b[i] = (byte)(i+32);
         }
         return b;
     }
