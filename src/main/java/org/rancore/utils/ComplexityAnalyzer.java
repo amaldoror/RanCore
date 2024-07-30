2aab02ba8d1d27bd938b4937d81968caaeafbe6c
Adrian Morgenthal <adrian.morgenthal92@gmail.com>
2024-07-30 15:47:24 +0200

    Update ComplexityAnalyzer

--- src/main/java/org/rancore/utils/ComplexityAnalyzer.java
+++ src/main/java/org/rancore/utils/ComplexityAnalyzer.java
@@ -2,7 +2,7 @@ package org.rancore.utils;
 
 public class ComplexityAnalyzer {
 
-    // Interface for a method that we want to analyze
+    // Interface for a method to analyze
     @FunctionalInterface
     public interface MethodToAnalyze {
         void execute();
@@ -24,21 +24,26 @@ public class ComplexityAnalyzer {
         long memoryUsed = endMemory - startMemory;
 
         // Print results
-        System.out.println("Time taken: " + duration + " nanoseconds");
-        System.out.println("Memory used: " + memoryUsed + " bytes");
+        System.out.println("Time:\t" + duration + " nanoseconds");
+        System.out.println("Memory:\t" + memoryUsed + " bytes");
     }
 
     public static void main(String[] args) {
         // Example usage:
         analyze(() -> sumArray(new int[]{1, 2, 3, 4, 5}));
+        analyze(() -> sumArray(new int[]{1,2,3,4,5,6,7,8,9,10}));
+        analyze(() -> sumArray(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}));
+        analyze(() -> sumArray(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30}));
     }
 
     // Example method to analyze
-    public static int sumArray(int[] array) {
+    public static void sumArray(int[] array) {
         int sum = 0;
         for (int num : array) {
+            for (int num2 : array) {
+                sum += num2;
+            }
             sum += num;
         }
-        return sum;
     }
 }
